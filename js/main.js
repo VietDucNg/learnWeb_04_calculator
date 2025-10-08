d766de63f5c01e6650bb76e6a699bb1f620c0c9a
VietDucNg <vietducnguyen.fit@gmail.com>
2025-10-08 15:27:53 +0200

    feat!: add ability to type number more than 1 digit
    add function concatNumber, so it concat current on-screen number with input number

--- js/main.js
+++ js/main.js
@@ -3,13 +3,13 @@ const operBtns = document.querySelectorAll('.operator');
 const equalBtn = document.querySelector('.equalBtn');
 const line2 = document.querySelector('.line2');
 
-function add(a,b) {return a + b};
+function add(a,b) {return Number(a) + Number(b)};
 function subtract(a,b) {return a - b};
 function multiply(a,b) {return a*b};
 function divide(a,b) {return a/b};
 function modulo(a,b) {return a%b};
 
-let firstNum = 0;
+let firstNum;
 let operator;
 let secondNum;
 
@@ -23,18 +23,43 @@ function operate(firstNum,operator,secondNum){
     }
 }
 
-function getCurrNum() {
+function getOnScreenNum() {
     return line2.textContent;
 }
 
-function updateLine2(content) {
+function updateScreen(content) {
     line2.textContent = content;
+}
+
+function isSecondNum() {
+    return (operator) ? true : false;
+}
+
+function concatNumber(inputNum) {
+    let number;
+    if (getOnScreenNum() == 0) return inputNum;
+    else {
+        let onScreenNum = String(getOnScreenNum());
+        number = onScreenNum + String(inputNum);
+    }
+    return Number(number);
+}
+
+function typeSecondNum(inputNum) {
+    updateScreen(0);
+    return concatNumber(inputNum)
 }
 
 function onClickNumBtns() {
     numBtns.forEach(btn => {
         btn.addEventListener('click', function(){
-            updateLine2(this.textContent);
+            if (isSecondNum()) {
+                let number = typeSecondNum(this.textContent); 
+                updateScreen(number);
+            } else {
+                let number = concatNumber(this.textContent)
+                updateScreen(number);
+            } 
         })
     });
 }
@@ -45,7 +70,7 @@ function saveOperator(btn) {
 
 function onClickOperBtns() {
     operBtns.forEach(btn => btn.addEventListener('click', function(){
-        firstNum = getCurrNum();
+        firstNum = getOnScreenNum();
         saveOperator(btn);
     }))
 }
@@ -54,12 +79,23 @@ function runOperation() {
     return operate(firstNum, operator, secondNum);
 }
 
+function resetOperation() {
+    operator = undefined;
+}
+
 function onClickEqualBtn () {
     equalBtn.addEventListener('click', function(){
-        secondNum = getCurrNum();
+        secondNum = getOnScreenNum();
         let result = runOperation();
-        updateLine2(result);
+        updateScreen(result);
+        resetOperation();
     })
+}
+
+function clear() {
+    line1.textContent = ''
+    line2.textContent = '0'
+
 }
 
 
